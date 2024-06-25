dd9115ab829d5551e42eea5aae723d2d14cceb91
HossamSadekk <66652255+HossamSadekk@users.noreply.github.com>
2024-06-25 22:41:36 +0200

    add project

---
+++ src/test/java/com/sideproject/mercatus/MercatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.mercatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
