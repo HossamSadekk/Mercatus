55431e37c62b3189639596f1fa3b4ebd6f7bf81a
HossamSadekk <66652255+HossamSadekk@users.noreply.github.com>
2024-06-26 20:25:20 +0200

    adding models

---
+++ mercatus/src/main/java/com/sideproject/mercatus/model/Product.java
@@ -0,0 +1,76 @@
+package com.sideproject.mercatus.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "short_discription", nullable = false)
+    private String shortDiscription;
+
+    @Column(name = "long_discription")
+    private String longDiscription;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @OneToOne(mappedBy = "product", cascade = CascadeType.REMOVE, optional = false, orphanRemoval = true)
+    private Inventory inventory;
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getLongDiscription() {
+        return longDiscription;
+    }
+
+    public void setLongDiscription(String longDiscription) {
+        this.longDiscription = longDiscription;
+    }
+
+    public String getShortDiscription() {
+        return shortDiscription;
+    }
+
+    public void setShortDiscription(String shortDiscription) {
+        this.shortDiscription = shortDiscription;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
